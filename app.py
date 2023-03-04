b991f0f6636a740f274b9b97511db101a8d5bd2c
dtparker19 <95888025+dtparker19@users.noreply.github.com>
2023-03-04 22:03:16 +0000

    init

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+from streamlit_chat import message
+from PIL import Image
+import os
+import openai 
+import json
+
+#retrive api key from json file
+with open("openai_api_key.json") as f:
+    api_key = json.load(f)
+#
+openai.api_key = api_key['API_KEY']
+
+def ask(question):
+    response = openai.Completion.create(
+               model="text-davinci-003",
+               prompt=question,
+               temperature=0.7,
+               max_tokens=1024,
+               stop=["\\n"],
+               top_p=1,
+               frequency_penalty=0,
+               presence_penalty=0
+               )
+    answer = response.choices[0].text
+    return answer
+
+#set the page title
+st.set_page_config(page_title="AI Teaching Assistant Bot", layout="centered")
+# set the image caption
+st.image(image, caption='Teaching Assitant Bot', use_column_width=True)
+# page header
+st.title(f"Teaching Assitant Bot")
+# store interaction history
+#
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+#
+
+# We will get the user's input by calling the get_text function
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text
+#
+user_input = get_text()
+#
+if user_input:
+    response = ask(user_input)
+    # store the output 
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(response)
+#
+if st.session_state['generated']:   
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
